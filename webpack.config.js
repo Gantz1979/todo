b8368eddbe7e219898eda3eb72f24975e831d139
Gantz <gantz.mbv@gmail.com>
2021-03-12 16:53:30 -0500

    inicio proyecto

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const HtmlWebPackPlugin       = require('html-webpack-plugin');
+const MiniCssExtractPlugin    = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CopyWebpackPlugin       = require('copy-webpack-plugin');
+ 
+ 
+module.exports = {
+
+    // mode: 'production',
+    mode: 'development',
+    optimization: {
+        minimizer: [new OptimizeCssAssetsPlugin()]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                exclude: /styles\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /styles\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+                options: {
+                    sources: false,
+                    minimize: false
+                },
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false
+                        }
+                    }
+                ]
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            ignoreOrder: false
+        }),
+        new CopyWebpackPlugin ({
+            patterns: [
+                { from: 'src/assets', to: 'assets/' },
+            ],
+        }),
+    ]
+ 
+    
+}
