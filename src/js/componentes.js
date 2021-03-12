68f5d596a899bd035426d46bbe17aa474e872e54
Gantz <gantz.mbv@gmail.com>
2021-03-12 16:57:00 -0500

    Primer commit

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -3,7 +3,7 @@ import '../css/componentes.css';
 
 export const saludar = (nombre) => {
 
-  console.log('Creando etiqueta H1 en el HTML PIN');
+  console.log('Creando etiqueta HHH1 en el HTML PIN');
 
   const h1 = document.createElement('h1');
   h1.innerHTML = `Hola tu nombre es: ${nombre}`;
